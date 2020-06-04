f127bda6aa3c51c3ede33598926cf66f1f4c51d3
Ashwini Rangaswamy <22480742+arangas27@users.noreply.github.com>
2020-06-03 19:35:13 -0700

    Update build-output.sh

--- build-output.sh
+++ build-output.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 echo -n "Docker Demo" && sleep 2 
-echo  -n "   Hello" World!! " && sleep 3 && echo -n "  Have a wonderful day!"
+echo -n "   Hello World!! " && sleep 3 && echo -n "  Have a wonderful day!"
